9f8c3cd589aa9adeb5bf5de075da6c32581552ef
rohithasura96 <rohit@hasura.io>
2023-02-08 18:34:09 +0530

    first commit calc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import react , {useState} from 'react';
+import {e, evaluate} from 'mathjs';
 
 function App() {
+  const [exp, setCount] = useState();
+  const Inc = (e) => {
+      setCount( exp + e.target.innerHTML);
+     
+  } 
+
+  
+  const clear = () => {
+    setCount('');
+    
+    
+  }
+  const cut = () => {
+    setCount(exp.slice(0, -1));
+  }
+
+  const calculate = () => {
+   let result = evaluate(exp);
+   let newCount =+ result ;
+    setCount(newCount);
+    
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="App">
+      <div className='input' >{exp}</div>
+      
+      <div className="row1">
+        <button onClick={clear}>C</button>
+        <button onClick={cut}>X</button>
+        <button onClick={Inc}>%</button>
+        <button onClick={Inc}>/</button>
+        <button onClick={Inc}>7</button>
+        <button onClick={Inc}>8</button>
+        <button onClick={Inc}>9</button>
+        <button onClick={Inc}>*</button>
+      
+        <button onClick={Inc}>4</button>
+        <button onClick={Inc}>5</button>
+        <button onClick={Inc}>6</button>
+        <button onClick={Inc}>-</button>
+       
+        <button onClick={Inc}>3</button>
+        <button onClick={Inc}>2</button>
+        <button onClick={Inc}>1</button>
+        <button onClick={Inc}>+</button>
+       </div>
+       <div className="rowend">
+         <button onClick={Inc}>0</button>
+         <button onClick={Inc}>.</button>
+         <button onClick={calculate}>=</button>
+        </div>
+      </div>
+
     </div>
+    
+    
+
   );
 }
 
